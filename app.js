48e848c504358c97b572242b5fd98f53e6d77e8d
Maxsedzik <maksymilian@maxsedzik.com>
2022-08-18 10:48:50 +0200

    dodane nowe

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+//SCALES AND NOTES
 let cmaj = ["c", "d", "e", "f", "g", "a", "b"];
-let cismaj = ["cis", "dis", "f", "fis", "gis", "ais", "c"];
+let cismaj = ["c#", "d#", "f", "f#", "g#", "a#", "c"];
 
 document.getElementById("find").addEventListener("click", finder);
 
 function finder() {
+  //SCRAPING NOTES FROM THE FORM
   let note1 = document.getElementById("note1").value;
   let note2 = document.getElementById("note2").value;
   let note3 = document.getElementById("note3").value;
   let note4 = document.getElementById("note4").value;
   let note5 = document.getElementById("note5").value;
 
+  //CHECKING WHETER INPUTS ARE REALLY NOTES OR NOT
+  let reControl = /^[a-gA-G]{1}#?$/;
+  console.log(reControl.test(note1));
+  if (reControl.test(note1)) {
+  } else {
+    document.getElementById("note1").classList.add("is-invalid");
+  }
+
+  //ARRAY MADE FROM NOTES
   let notes = [];
   notes.push(note1);
   notes.push(note2);
@@ -17,16 +28,66 @@ function finder() {
   notes.push(note4);
   notes.push(note5);
 
-  let re1 = new RegExp(note1);
-  let re2 = new RegExp(note2);
-  let re3 = new RegExp(note3);
-  let re4 = new RegExp(note4);
-  let re5 = new RegExp(note5);
+  //CLEANING ARRAY FROM EMPTY ELEMENTS
+  let notesCleared = notes.filter((note) => note !== "");
 
-  if (note1.length >= 1) {
-    if (re1.test(cmaj) === true) {
-    
-  } else {
-    console.log("CMAJ BAD");
+  //CREATING REGEXP FROM NOTES NOMATTER THE CASE
+  let re1 = new RegExp(note1, "i");
+  let re2 = new RegExp(note2, "i");
+  let re3 = new RegExp(note3, "i");
+  let re4 = new RegExp(note4, "i");
+  let re5 = new RegExp(note5, "i");
+
+  //CHECKING THE NOTES WITH IF STATEMENTS
+
+  //CMAJ
+  if (notesCleared.length >= 1) {
+    if (re1.test(cmaj)) {
+      if (re2.test(cmaj)) {
+        if (re3.test(cmaj)) {
+          if (re4.test(cmaj)) {
+            if (re5.test(cmaj)) {
+              console.log("cmaj good");
+            } else {
+              console.log("cmaj bad");
+            }
+          } else {
+            console.log("cmaj bad");
+          }
+        } else {
+          console.log("cmaj bad");
+        }
+      } else {
+        console.log("cmaj bad");
+      }
+    } else {
+      console.log("cmaj bad");
+    }
+  }
+
+  //C#MAJ
+
+  if (notesCleared.length >= 1) {
+    if (re1.test(cismaj)) {
+      if (re2.test(cismaj)) {
+        if (re3.test(cismaj)) {
+          if (re4.test(cismaj)) {
+            if (re5.test(cismaj)) {
+              console.log("cismaj good");
+            } else {
+              console.log("cismaj bad");
+            }
+          } else {
+            console.log("cismaj bad");
+          }
+        } else {
+          console.log("cismaj bad");
+        }
+      } else {
+        console.log("cismaj bad");
+      }
+    } else {
+      console.log("cismaj bad");
+    }
   }
 }
